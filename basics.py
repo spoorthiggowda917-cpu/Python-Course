ae054f70754bdcc51612293fdc5af7aa7f8d78eb
Spoorthi G <spoorthiggowda917@gmai.com>
2026-02-03 21:18:00 +0530

    first commit

--- basics.py
+++ basics.py
@@ -1,1 +1,61 @@
 print("Hello World")
+
+#>>>Type Conversion
+age=20
+s="2"
+print(age+int(s))
+
+#>>>Arithmetic Operations
+a=10
+b=3
+print(a+b) #Addition
+print(a-b) #Subtraction
+print(a*b) #Multiplication
+print(a/b) #Division
+print(a//b) #Floor Division
+print(a%b)  #Modulus|Remainder
+print(a**b) #Exponential
+
+"""#>>>input/ouput 
+age=input("Wt is ur age: ")
+print(age)
+
+'''2.1 Common String Operations
+#Concatenation
+>>>Concatenate the string'''
+message=input("Enter ur mssg : ")
+name=input("Enter u r name : ")
+print(message + " This is " + name)
+
+boy_name=input("Enter boy name: ")
+boy_age=int(input("Enter age"))
+girl_name=input("Enter girl name: ")
+girl_age=int(input("Enter age"))
+age_diff= abs(boy_age-girl_age) #absolute value= gives only exact value but not -ve values
+
+print(f"{boy_name} and {girl_name}, Age difference is str({age_diff})") """
+
+#Repitition
+greeting ="Hello! "
+print(greeting*6)
+
+#2.2 String Methods
+#upper()
+print(greeting.upper())
+#lower()
+print(greeting.lower())
+#strip =>removes extra spaces
+print(greeting.strip())
+#replace
+print(greeting.replace("Hello","Greetings"))
+#length
+print(len(greeting))
+
+#2.3 Accessing string characters
+name="Spoorthi"
+print(name[5])
+print(name[1:8]) #[start,ends+1]
+print(name[:5])
+print(name[1:])
+print(name[-4])
+print(name[::2])
