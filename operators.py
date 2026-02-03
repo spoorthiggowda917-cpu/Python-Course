900f6dd05a4b04ada8fbe9fb43e96da16c17ff8f
Spoorthi G <spoorthiggowda917@gmai.com>
2026-02-03 21:52:14 +0530

    first commit

---
+++ operators.py
@@ -0,0 +1,40 @@
+#2.1 Assignment Operators
+x=50
+print(x)
+x+=5
+print(x)
+x-=5
+print(x)
+x*=5
+print(x)
+x/=5
+print(x)
+print("---------------------")
+#2.2 Comparison Operators
+a=10
+b=20
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=30)
+print(b>=40)
+
+print("---------------------")
+#Logical Operators
+print(True and True)
+print(True or False)
+print(not(True))
+print("---------------------")
+
+#Membership Operators
+my_string="Spoorthi"
+my_string1="Spoorthig"
+print("p" in my_string)
+print("p" not in my_string)
+
+print(("S" in my_string) and ("S" in my_string1))
+print(("g" in my_string) or ("g" in my_string1))
+print(not("g" in my_string))
+
+#Bitwise Operators => &,|,^,~,<<,>>
