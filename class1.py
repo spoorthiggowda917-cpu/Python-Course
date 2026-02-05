e520503104e5251b30b1e4c7720f33d34bbe1a2f
Spoorthi G <spoorthiggowda917@gmai.com>
2026-02-05 13:03:40 +0530

    first commit

--- class1.py
+++ class1.py
@@ -1,3 +1,4 @@
+"""#CLASS1
 #constants
 MAX_Count=1
 MAX_COUNT=22
@@ -45,4 +46,62 @@ print(d.split("n")) #splis at the char mentioned
 #strip
 s1="         replace      "
 print(s1.rstrip(" ")) #removes right spcaes in string
-print(s1.lstrip(" ")) #removes left
+print(s1.lstrip(" ")) #removes left"""
+
+
+#CLASS2
+#Lists
+a=[1,2,3]
+a.extend([4,5]) #to add more than 1 elements at the end
+print(a)
+a.extend(["string"])
+print(a)
+a.insert(1,8)
+print(a)
+print("--------------")
+b=[5,6,7]
+b.insert(9,8) #insert can be also added at last
+print(b)
+b.pop(2)
+print(b)
+print("--------------")
+b1=[5,6,7,8,4,3,2,1,1,1,10,2,5,8]
+b1.remove(8) #removes only first occured number,if its repeated
+print(b1)
+#filter(b1)  => doubt
+b1.clear()
+print(b1)
+print("--------------")
+list1=[4,8,5,6,7,9,2,1,3]
+list1.sort()
+print(list1)
+
+list1.sort(reverse=True) #to sort in descending order
+print(list1)
+
+print("--------------")
+list2=sorted(list1) # sorted is not a iterable function in list
+print(list2)
+list3=sorted(list1,reverse=True)
+print(list3)
+
+rev=[7,8,9,2,5,4,6,3,1]
+rev.reverse() #reverses but wont be sorted
+print(rev)
+
+print("--------------")
+a=[1,2]
+b=[3,4]
+c=[5,6,7]
+a.extend(b) #to add two lists together but not as nested list
+print(a+b) #it created new list but wont chnage original list
+print(a)
+
+print("--------------")
+a=[1,2,3,4]
+b=a.copy()
+b.append(5)
+print(b)
+print(a)
+""" if we use copy "a" will not be changed
+but if we use b=a, a can be changed originally """
